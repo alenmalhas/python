bbab6b93ac6f8c48ba7e5f2873057f2c38db0c0a
Alen M <alen@m>
2024-02-23 14:06:22 +0000

    pgvector-django\djangoPGVectorApp1\pgvectorapp1\.vscode\launch.json pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\__init__.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\admin.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\apps.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\models.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\templates\base.html pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\templates\index.html pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\tests.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\urls.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\core\views.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\manage.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\pgvectorapp1\__init__.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\pgvectorapp1\asgi.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\pgvectorapp1\settings.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\pgvectorapp1\urls.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\pgvectorapp1\wsgi.py pgvector-django\djangoPGVectorApp1\pgvectorapp1\mysite\requirements.txt pgvector-django\djangoPGVectorApp1\pgvectorapp2\.vscode\launch.json pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\db.sqlite3 pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\manage.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\mysite\__init__.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\mysite\asgi.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\mysite\settings.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\mysite\urls.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\mysite\wsgi.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\__init__.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\admin.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\apps.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\migrations\0001_initial.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\migrations\__init__.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\models.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\tests.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\urls.py pgvector-django\djangoPGVectorApp1\pgvectorapp2\mysite\polls\views.py

---
+++ pgvector-django/djangoPGVectorApp1/pgvectorapp1/mysite/core/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from core.models import LangchainPgEmbedding
+from pgvector.django import L2Distance
+
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        input_text = request.POST.get('input_text')
+        input_embedding = QueryModelWithTextChunks(input_text)
+        most_similar_doc = LangchainPgEmbedding.objects.order_by(
+            L2Distance('embedding', input_embedding)
+        ).first()
+        context = {
+            'input_text': input_text,
+            'most_similar' : most_similar_doc
+        }
+        return render(request, 'index.html', context)
+    
+    embeddings = LangchainPgEmbedding.objects.all()
+    doc = embeddings.first().document
+    t1 =doc.title()
+    context = {'embeddings':embeddings}
+    return render(request, 'index.html', context)
+
+def get_embedding(text):
+    return text
+
+
+import requests
+import os
+
+model_id = "sentence-transformers/all-MiniLM-L6-v2"
+api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+hf_token = os.getenv('HF_ACCESS_TOKEN')
+headers = {"Authorization": f"Bearer {hf_token}"}
+
+def QueryModelWithTextChunks(texts):
+    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
+    return response.json()
