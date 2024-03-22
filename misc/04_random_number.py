ee763f2e2414a3f03a7185921823f66f165a9412
Alen M <alen@m>
2024-03-22 17:06:38 +0000

    updated misc/04_random_number.py

--- misc/04_random_number.py
+++ misc/04_random_number.py
@@ -4,15 +4,21 @@ def random_number1():
     r = random.randint(0, 100)
     return r
     
-#python -m unittest 01_capital_indexes.py
-import unittest
+def random_number2():
+    r = random.randrange(0, 100, 2)
+    return r
+
+def random_number3():
+    r = random.randrange(0, 100, 5)
+    return r
 
 def random_number():
-    r1 =  random_number1()
+    r1 =  random_number3()
     print(f'Generated random no: {r1}')
     return r1
 
-
+#python -m unittest 01_capital_indexes.py
+import unittest
 class TestsFormid(unittest.TestCase):
     def test_random_number_returns_between_1_and_100(self):
         actual = random_number()
