180a4052c7726f700d544b3581317709f6cebcb2
Alen M <alen@m>
2024-03-28 16:56:20 +0000

    misc\HR_240328_sherlock_and_the_beast_test.py

--- misc/HR_240328_sherlock_and_the_beast_test.py
+++ misc/HR_240328_sherlock_and_the_beast_test.py
@@ -35,3 +35,18 @@ class TestCases(unittest.TestCase):
         expected = "555"+"555"+"555"+"333"+"33"
         self.assertNotEqual(actual, -1)
         self.assertEqual(actual, expected)
+
+    def test_cases_all_return_correct_result(self):
+        dictInputExpectedOutput = { 
+            1:"-1", 
+            3:"555",
+            5:"33333",
+            11:"55555533333",
+            13:"5553333333333",
+            15:"555555555555555"
+        }
+        for input in dictInputExpectedOutput.keys():
+            expected = dictInputExpectedOutput[input]
+            with self.subTest("dictInputExpectedOutput tests return True", input=input):
+                actual =  mf.sherlockBeastNaive(input)
+                self.assertEqual(actual, expected)
